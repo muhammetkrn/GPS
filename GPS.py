32d150f88d0ea34a1d551aa3dede7cf3a21c0afa
Muhammet KIRAN <43640752+muhammetkrn@users.noreply.github.com>
2019-09-04 12:00:52 +0300

    Create GPS.py

---
+++ GPS.py
@@ -0,0 +1,8 @@
+import requests
+
+ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+my_ip = ip_request.json()['ip']
+geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
+geo_request = requests.get(geo_request_url)
+geo_data = geo_request.json()
+print(geo_data['organization'])
